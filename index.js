be4b6d5484482bd383c21959eb44f5d7ab842b6a
danususyanto02 <danu.mds1402@gmail.com>
2022-10-27 13:18:07 +0700

    Update Node

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    app = express(),
+    PORT = 8080,
+    keyAction = require("robotjs"),
+    sayText = require('say');
+
+
+app.use(bodyParser.json());
+
+app.post("/", (request, respond) => {
+
+    // amount_raw itu jumlah donasi
+    // donator_nama itu nama si donaturnya
+    // donator_email itu email si donatornya
+    // message itu pesan dari donator
+
+    sayText.speak('Donasi ' + request.body.amount_raw + 'Dari ' + request.body.donator_name);
+    console.log('Donasi Dari: ' + request.body.donator_name);
+    console.log('Donasi Sebesar: ' + request.body.amount_raw);
+                                       
+    var timer = Date.now();
+
+    if (request.body.amount_raw >= 10000 && request.body.amount_raw <= 15000) {
+
+        while ((Date.now() - timer) < 10000) {
+            keyAction.keyTap("1");
+            keyAction.keyTap("2");
+            keyAction.keyTap("3");
+        }
+
+    } else if (request.body.amount_raw > 15000 && request.body.amount_raw <= 25000) {
+
+        while ((Date.now() - timer) < 10000) {
+            keyAction.keyTap("g");
+        }
+
+    } else if (request.body.amount_raw > 25000 && request.body.amount_raw <= 35000) {
+
+        keyAction.keyTap("X");
+
+    } else if (request.body.amount_raw > 35000 && request.body.amount_raw <= 50000) {
+
+        while ((Date.now() - timer) < 10000) {
+            keyAction.keyToggle('w', 'down');
+        }
+        keyAction.keyToggle('w', 'up');
+
+    } else if (request.body.amount_raw > 50000 && request.body.amount_raw <= 70000) {
+
+        while ((Date.now() - timer) < 10000) {
+            keyAction.keyTap("space");
+        }
+       
+
+    } else if (request.body.amount_raw >= 90000) {
+
+        keyAction.keyTap("f4", ["alt"]);
+        keyAction.moveMouse(594, 560);
+        keyAction.mouseClick(button = "left");
+
+    }
+
+    respond.status(200);
+})
+
+app.listen(PORT, () => console.log(`Program Berjalan Pada Port: ${PORT}`));
---
+++ readme.md
@@ -0,0 +1,49 @@
+## About The Project | Tentang Project
+EN: Webhook To Get Notification Saweria Donation.
+
+ID: Webhook Untuk Mendapat Notifikasi Donasi Saweria.
+
+Youtube: https://youtu.be/bAvIS6-exZg
+
+
+
+### Prerequisites | Yang Diperlukan 
+
+EN: Required before using the program code.
+
+ID: Yang diperlukan sebelum menggunakan kode program.
+
+* Node JS https://nodejs.org/en/download/
+* Ngrok https://ngrok.com/download
+* Saweria Account | Akun Saweria
+
+
+## Action From Notification | Aksi Dari Notifikasi
+
+
+```
+- Donation | Donasi Rp. 10.000  - Rp.15.000
+    EN: Changing Weapon For 10 Second
+    ID: Mengganti Senjata Selama 10 Detik
+
+- Donation | Donasi Rp. 15.001  - Rp.25.000
+    EN: Dropped Weapon And Spike For 10 Second
+    ID: Menjatuhkan Senjata Dan Spike Selama 10 Detik
+
+- Donation | Donasi Rp. 25.001  - Rp.35.000
+    EN: Use Ultimate Abilites
+    ID: Menggunakan Ulti
+
+- Donation | Donasi Rp. 35.001  - Rp.50.000
+    EN: Forward For 10 Second
+    ID: Jalan Maju Selama 10 Detik
+
+- Donation | Donasi Rp. 50.001  - Rp.70.000
+    EN: Jump For 10 Second
+    ID: Lompat Selama 10 Detik
+
+- Donation | Donasi Rp. 90.000++
+    EN: Quit Valorant (ALT + F4)
+    ID: Keluar Dari Valorant (ALT + F4)
+
+```
